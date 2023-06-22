e6d505cc778a2af7c9e962e15ec0037ad256b208
Mark Padilla <markjjpadilla@gmail.com>
2023-06-21 21:35:27 -0400

    Remove util files from old unoptimized implementation
    See commit 22ade6aaed3b08ec8d5d3fd9305124c99c648c0d for an explanation
    of why the implementation has changed.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,4 @@
 pub mod format_special_chars {
-    pub mod em_dash;
-    pub mod ellipsis;
-
     mod format_special_char;
     pub mod consecutive_chars_to_special_char_formatter;
     pub mod special_char_pair_formatter;
